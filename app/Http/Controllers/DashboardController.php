9a2701c6156a52519e72cb40cc4ac02401387d77
Nhật An <nhatan@MacBook-Pro.local>
2021-10-19 08:31:42 +0700

    Change view dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index() {
-        return view('admin.index');
+    public function index()
+    {
+        return view('admin.home');
     }
 }
