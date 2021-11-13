f2883b16cdd721f75fdbafece6ed7c2e04c7ae75
Nhật An <nhatan14042000@gmail.com>
2021-11-13 18:05:15 +0700

    Add fillable

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Posts extends Model
 {
     use HasFactory;
+    protected $table = 'posts';
+    protected $fillable = ['title', 'description', 'content', 'slug', 'thumbnail', 'user_id'];
 }
