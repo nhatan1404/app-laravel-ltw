ae5b76aa70d730d682d977e8f7941f93fc06d72c
Nhật An <nhatan14042000@gmail.com>
2021-11-13 18:05:36 +0700

    Move line table to up

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'description', 'slug', 'parent_id'];
     protected $table = 'categories';
+    protected $fillable = ['title', 'description', 'slug', 'parent_id'];
 }
