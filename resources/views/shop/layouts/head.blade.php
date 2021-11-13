80231aa9f393a4e50e4386d69c7f5b6f9f43a5a8
Nhật An <nhatan14042000@gmail.com>
2021-11-13 19:18:05 +0700

    Add view shop head

---
+++ resources/views/shop/layouts/head.blade.php
@@ -0,0 +1,30 @@
+<head>
+  <title>@yield('title')</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap"
+  rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="{{asset('shop/css/open-iconic-bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('shop/css/animate.css')}}">
+
+<link rel="stylesheet" href="{{asset('shop/css/owl.carousel.min.css')}}">
+<link rel="stylesheet" href="{{asset('shop/css/owl.theme.default.min.css')}}">
+<link rel="stylesheet" href="{{asset('shop/css/magnific-popup.css')}}">
+
+<link rel="stylesheet" href="{{asset('shop/css/aos.css')}}">
+
+<link rel="stylesheet" href="{{asset('shop/css/ionicons.min.css')}}">
+
+<link rel="stylesheet" href="{{asset('shop/css/bootstrap-datepicker.css')}}">
+<link rel="stylesheet" href="{{asset('shop/css/jquery.timepicker.css')}}">
+
+
+<link rel="stylesheet" href="{{asset('shop/css/flaticon.css')}}">
+<link rel="stylesheet" href="{{asset('shop/css/icomoon.css')}}">
+<link rel="stylesheet" href="{{asset('shop/scss/style.css')}}">
+@stack('styles')
+</head>
