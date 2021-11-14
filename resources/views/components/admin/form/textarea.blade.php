070d4e2a2d1e04d2ebc6f025fd4ec7504ae148fa
Nhật An <nhatan14042000@gmail.com>
2021-11-14 20:16:05 +0700

    Fix rows columns

--- resources/views/components/admin/form/textarea.blade.php
+++ resources/views/components/admin/form/textarea.blade.php
@@ -3,15 +3,15 @@
     'property' => '',
     'placeholder' => '',
     'value' => null,
-    'rows' => '0',
-    'columns' => '0',
+    'rows' => 0,
+    'columns' => 0,
 ])
 
 <div class="form-group">
     <label for="input{{ ucfirst($property) }}" class="col-form-label">{{ $name }}: </label>
     <textarea class="form-control" id="input{{ ucfirst($property) }}" name="{{ $property }}"
-        placeholder="{{ $placeholder }}"{{ $rows > 0 ? ' rows="' + $rows + '"' : '' }}
-        {{ $columns > 0 ? ' columns="' + $columns + '"' : '' }}>{{ $value }}</textarea>
+        placeholder="{{ $placeholder }}"
+        {{ $rows > 0 ? ' rows=' . $rows . '' : '' }}{{ $columns > 0 ? ' columns=' . $columns . '' : '' }}>{{ $value }}</textarea>
     @error($property)
         <span class="text-danger">{{ $message }}</span>
     @enderror
