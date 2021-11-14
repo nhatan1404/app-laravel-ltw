c440a7f4eb018fa0769cfe979fe546be8f8b1e9e
Nhật An <nhatan14042000@gmail.com>
2021-11-14 13:20:18 +0700

    Add relation posts category

--- app/Models/PostsCategory.php
+++ app/Models/PostsCategory.php
@@ -9,5 +9,29 @@ class PostsCategory extends Model
 {
     use HasFactory;
     protected $table = 'posts_categories';
-    protected $fillable = ['title', 'slug', 'parent_id'];
+    protected $fillable = ['title', 'description', 'slug', 'parent_id'];
+
+    public function posts()
+    {
+        return $this->hasMany('App\Models\Posts', 'category_id', 'id')->where('status', 'active');
+    }
+
+    public function parent()
+    {
+        return $this->hasMany('App\Models\PostsCategory', 'parent_id')->where('status', 'active');
+    }
+
+    public function children(){
+        return $this->hasMany('App\Models\PostsCategory','parent_id','id');
+    }
+
+    public static function getParentCategories()
+    {
+        return PostsCategory::whereNull('parent_id')->orderBy('title', 'ASC')->get();
+    }
+
+    public static function getListByParent()
+    {
+        return PostsCategory::with('children')->whereNull('parent_id')->get();
+    }
 }
