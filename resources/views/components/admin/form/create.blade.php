116147a76de46ba58deaa46d255b149c8f629386
Nhật An <nhatan14042000@gmail.com>
2021-11-14 19:23:19 +0700

    Add view component create page

---
+++ resources/views/components/admin/form/create.blade.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-lg-10 mx-auto">
+        <div class="card">
+            <h5 class="card-header">Tạo {{ $name }}</h5>
+            <div class="card-body">
+                <form method="post" action="{{ route($route) }}">
+                    {{ csrf_field() }}
+                    {{ $slot }}
+                    <div class="form-group mb-3">
+                        <button type="reset" class="btn btn-warning">Xoá</button>
+                        <button class="btn btn-success" type="submit">Tạo</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
