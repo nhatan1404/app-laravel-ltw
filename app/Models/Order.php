8f1f26586964318f8539e3282f7d7ae710a03844
Nhật An <nhatan14042000@gmail.com>
2021-11-14 18:06:22 +0700

    Fix name table

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
-    protected $table = 'order';
+    protected $table = 'orders';
     protected $fillable = ['firstname', 'lastname', 'address', 'telephone', 'email', 'note', 'tottal'];
 }
