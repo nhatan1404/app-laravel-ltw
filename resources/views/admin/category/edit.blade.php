89a6287a9ff4b99e6ffd11cb331434814d161554
Nhật An <nhatan14042000@gmail.com>
2021-11-14 19:22:00 +0700

    Updare using component

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -1,65 +1,38 @@
 @extends('admin.layouts.app')
 @section('title', 'Sửa Danh Mục Sản Phẩm')
 @section('content')
-<div class="row">
-  <div class="col-lg-10 mx-auto">
-    <div class="card">
-      <h5 class="card-header">Sửa Danh Mục</h5>
-      <div class="card-body">
-        <form method="post" action="{{route('category.update',$category->id)}}">
-          @csrf
-          @method('PATCH')
-          <div class="form-group">
-            <label for="inputTitle" class="col-form-label">Tiêu đề: <span class="text-danger">*</span></label>
-            <input id="inputTitle" type="text" name="title" placeholder="Nhập tiêu đề" value="{{$category->title}}" class="form-control">
-            @error('title')
-            <span class="text-danger">{{$message}}</span>
-            @enderror
-          </div>
+    <x-Admin.Form.Edit name="Danh Mục" route="category.update" :id="$category->id">
+        <x-Admin.Form.Input name="Tiêu đề" property="title" placeholder="Nhập tiêu đề" value="{{ $category->title }}" />
 
-          <div class="form-group">
-            <label for="inputDescription" class="col-form-label">Mô tả: </label>
-            <textarea class="form-control" id="inputDescription" name="description">{{$category->description}}</textarea>
-            @error('description')
-            <span class="text-danger">{{$message}}</span>
-            @enderror
-          </div>
+        <x-Admin.Form.Textarea name="Mô tả" property="description" placeholder="Nhập mô tả"
+            value="{{ $category->description }}" placeholder="Nhập mô tả" />
 
-          <div class="form-group {{(($category->parent_id==null) ? 'd-none' : '')}}">
+        <div class="form-group {{ $category->parent_id == null ? 'd-none' : '' }}">
             <label for="parent_id">Danh Mục Lớn: <small>(tuỳ chọn)</small></label>
             <select name="parent_id" class="form-control">
-              <option value="">Chọn danh mục</option>
-              @foreach($parentCategories as $key=>$parentCategory)
-              <option value='{{$parentCategory->id}}' {{(($parentCategory->id==$category->parent_id) ? 'selected' : '')}}>{{$parentCategory->title}}</option>
-              @endforeach
+                <option value="">Chọn danh mục</option>
+                @foreach ($parentCategories as $key => $parentCategory)
+                    <option value='{{ $parentCategory->id }}'
+                        {{ $parentCategory->id == $category->parent_id ? 'selected' : '' }}>{{ $parentCategory->title }}
+                    </option>
+                @endforeach
             </select>
-          </div>
-
-          <div class="form-group mb-3">
-            <button class="btn btn-success" type="submit">Cập Nhật</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
-@endsection
-
-@push('styles')
-<link rel="stylesheet" href="{{asset('backend/summernote/summernote.min.css')}}">
+        </div>
+    </x-Admin.Form.Edit> @endsection @push('styles')
+    <link rel="stylesheet" href="{{ asset('backend/summernote/summernote.min.css') }}">
 @endpush
 @push('scripts')
-<script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
-<script src="{{asset('backend/summernote/summernote.min.js')}}"></script>
-<script>
-  $('#is_parent').change(function() {
-    var is_checked = $('#is_parent').prop('checked');
-    if (is_checked) {
-      $('#parent_cat_div').addClass('d-none');
-      $('#parent_cat_div').val('');
-    } else {
-      $('#parent_cat_div').removeClass('d-none');
-    }
-  })
-</script>
+    <script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
+    <script src="{{ asset('backend/summernote/summernote.min.js') }}"></script>
+    <script>
+        $('#is_parent').change(function() {
+            var is_checked = $('#is_parent').prop('checked');
+            if (is_checked) {
+                $('#parent_cat_div').addClass('d-none');
+                $('#parent_cat_div').val('');
+            } else {
+                $('#parent_cat_div').removeClass('d-none');
+            }
+        })
+    </script>
 @endpush
