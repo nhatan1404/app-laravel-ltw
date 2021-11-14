09fd5e0dc8af8bc6f706f755c218681eac0c3de0
Nhật An <nhatan14042000@gmail.com>
2021-11-14 13:19:37 +0700

    Add relation category, author and method get list

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -9,5 +9,17 @@ class Posts extends Model
 {
     use HasFactory;
     protected $table = 'posts';
-    protected $fillable = ['title', 'description', 'content', 'slug', 'thumbnail', 'user_id'];
+    protected $fillable = ['title', 'description', 'content', 'slug', 'thumbnail', 'status', 'category_id', 'user_id'];
+
+    public function category(){
+        return $this->hasOne('App\Models\PostsCategory','id','category_id');
+    }
+
+    public function author(){
+        return $this->hasOne('App\Models\User','id','user_id');
+    }
+
+    public static function getList() {
+        return Posts::with(['category','author'])->orderBy('id','DESC')->paginate(15);
+    }
 }
