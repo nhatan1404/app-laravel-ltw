51960d660233b81f7012b3a2e634387f15e6ef1f
Nhật An <nhatan14042000@gmail.com>
2021-11-14 19:21:52 +0700

    Updare using component

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -1,46 +1,20 @@
 @extends('admin.layouts.app')
+@section('title', 'Tạo Danh Mục')
 @section('content')
-<div class="row">
-  <div class="col-lg-10 mx-auto">
-    <div class="card">
-      <h5 class="card-header">Tạo Danh Mục</h5>
-      <div class="card-body">
-        <form method="post" action="{{route('category.store')}}">
-          {{csrf_field()}}
-          <div class="form-group">
-            <label for="inputTitle" class="col-form-label">Tiêu đề: <span class="text-danger">*</span></label>
-            <input id="inputTitle" type="text" name="title" placeholder="Nhập tiêu đề" value="{{old('title')}}" class="form-control">
-            @error('title')
-            <span class="text-danger">{{$message}}</span>
-            @enderror
-          </div>
+    <x-Admin.Form.Create name='Danh Mục' route='category.create'>
+        <x-Admin.Form.Input name="Tiêu đề" property="title" placeholder="Nhập tiêu đề" value="{{ old('title') }}" />
 
-          <div class="form-group">
-            <label for="inputDescription" class="col-form-label">Mô tả: </label>
-            <textarea class="form-control" id="inputDescription" name="description">{{old('description')}}</textarea>
-            @error('description')
-            <span class="text-danger">{{$message}}</span>
-            @enderror
-          </div>
+        <x-Admin.Form.Textarea name=" Mô tả" property="description" placeholder="" value="{{ old('description') }}"
+            placeholder="Nhập mô tả" />
 
-          <div class="form-group {{(count($parentCategories) == 0) ? 'd-none' : ''}}">
+        <div class="form-group {{ count($parentCategories) == 0 ? 'd-none' : '' }}">
             <label for="parent_id">Danh Mục Lớn: <small>(tuỳ chọn)</small></label>
             <select name="parent_id" class="form-control">
-              <option value="">Chọn danh mục</option>
-              @foreach($parentCategories as $key=>$parentCategory)
-              <option value='{{$parentCategory->id}}'>{{$parentCategory->title}}</option>
-              @endforeach
+                <option value="">Chọn danh mục</option>
+                @foreach ($parentCategories as $key => $parentCategory)
+                    <option value='{{ $parentCategory->id }}'>{{ $parentCategory->title }}</option>
+                @endforeach
             </select>
-          </div>
-
-          <div class="form-group mb-3">
-            <button type="reset" class="btn btn-warning">Xoá</button>
-            <button class="btn btn-success" type="submit">Tạo</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
-
+        </div>
+    </x-Admin.Form.Create>
 @endsection
