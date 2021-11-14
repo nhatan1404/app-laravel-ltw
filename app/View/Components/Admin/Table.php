38c06f9ab1861d7d99e54b9b4d3b16f82fc58f8a
Nhật An <nhatan14042000@gmail.com>
2021-11-14 18:05:40 +0700

    Add component admin table

---
+++ app/View/Components/Admin/Table.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\View\Components\Admin;
+
+use Illuminate\View\Component;
+
+class Table extends Component
+{
+    public $name;
+    public $columns;
+    public $create;
+    public $value;
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($name, $columns, $create, $value)
+    {
+        $this->name = $name;
+        $this->columns = $columns;
+        $this->create = $create;
+        $this->value = $value;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.admin.table');
+    }
+}
