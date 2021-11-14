85d6792f953b98fc4500f240c33f8ca3662266e0
Nhật An <nhatan14042000@gmail.com>
2021-11-14 13:14:31 +0700

    Update sidebar item

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,62 +1,93 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-  <!-- Sidebar - Brand -->
-  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('dashboard.index')}}">
-    <div class="sidebar-brand-icon rotate-n-15">
-      <i class="fas fa-laugh-wink"></i>
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard.index') }}">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">DASHBOARD</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard.index') }}  ">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        SHOP
     </div>
-    <div class="sidebar-brand-text mx-3">DASHBOARD</div>
-  </a>
 
-  <!-- Divider -->
-  <hr class="sidebar-divider my-0">
+    <!-- Nav Itemm - Category Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('product.index') }}">
+            <i class="fas fa-shopping-cart"></i>
+            <span>Sản Phẩm</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('category.index') }}">
+            <i class="far fa-list-alt"></i>
+            <span>Danh Mục Sản Phẩm</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('order.index') }}">
+            <i class="fas fa-cart-arrow-down"></i>
+            <span>Đơn Đặt Hàng</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('voucher.index') }}">
+            <i class="fas fa-file-invoice-dollar"></i>
+            <span>Mã Giảm Giá</span></a>
+    </li>
 
-  <!-- Nav Item - Dashboard -->
-  <li class="nav-item">
-    <a class="nav-link" href="{{route('dashboard.index')}}  ">
-      <i class="fas fa-fw fa-tachometer-alt"></i>
-      <span>Dashboard</span></a>
-  </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider">
 
-  <!-- Divider -->
-  <hr class="sidebar-divider">
+    <div class="sidebar-heading">
+        BÀI VIẾT
+    </div>
 
-  <!-- Heading -->
-  <div class="sidebar-heading">
-    SHOP
-  </div>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('posts.index') }}">
+            <i class="fas fa-book"></i>
+            <span>Bài Viết</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('posts-category.index') }}">
+            <i class="fas fa-poll-h"></i>
+            <span>Danh Mục Bài Viết</span></a>
+    </li>
 
-  <!-- Nav Itemm - Category Collapse Menu -->
-  <li class="nav-item">
-    <a class="nav-link" href="{{route('voucher.index')}}">
-      <i class="fas fa-file-invoice-dollar"></i>
-      <span>Mã Giảm Giá</span></a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="{{route('category.index')}}">
-      <i class="far fa-list-alt"></i>
-      <span>Danh Mục</span></a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="{{route('product.index')}}">
-      <i class="fas fa-shopping-cart"></i>
-      <span>Sản Phẩm</span></a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="{{route('user.index')}}">
-      <i class="fas fa-users"></i>
-      <span>Tài Khoản</span></a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="{{route('file-manager')}}">
-      <i class="fas fa-folder-open"></i>
-      <span>Quản Lý Tập Tin</span></a>
-  </li>
-  <!-- Divider -->
-  <hr class="sidebar-divider d-none d-md-block">
-  <!-- Sidebar Toggler (Sidebar) -->
-  <div class="text-center d-none d-md-inline">
-    <button class="rounded-circle border-0" id="sidebarToggle"></button>
-  </div>
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <div class="sidebar-heading">
+        CHUNG
+    </div>
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('user.index') }}">
+            <i class="fas fa-users"></i>
+            <span>Tài Khoản</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('file-manager') }}">
+            <i class="fas fa-folder-open"></i>
+            <span>Quản Lý Tập Tin</span></a>
+    </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
 
 </ul>
