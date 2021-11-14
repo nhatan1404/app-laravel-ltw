a6b7fd92a583b4408092ed5c5731c126375bed29
Nhật An <nhatan14042000@gmail.com>
2021-11-14 19:20:29 +0700

    Add component input

---
+++ app/View/Components/Admin/Form/Input.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\View\Components\Admin\Form;
+
+use Illuminate\View\Component;
+
+class Input extends Component
+{
+    public $name;
+    public $property;
+    public $placeholder;
+    public $value;
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($name, $property, $placeholder, $value)
+    {
+        $this->name = $name;
+        $this->property = $property;
+        $this->placeholder = $placeholder;
+        $this->value = $value;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.admin.form.input');
+    }
+}
