557e2d59db53055c38c7e643542b5a779dc0049c
Nhật An <nhatan14042000@gmail.com>
2021-11-14 13:19:57 +0700

    Add relation products, parent, children

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,4 +10,24 @@ class Category extends Model
     use HasFactory;
     protected $table = 'categories';
     protected $fillable = ['title', 'description', 'slug', 'parent_id'];
+
+    public function products()
+    {
+        return $this->hasMany('App\Models\Product', 'category_id', 'id')->where('status', 'active');
+    }
+
+    public function parent()
+    {
+        return $this->hasMany('App\Models\Category', 'parent_id')->where('status', 'active');
+    }
+
+    public function children()
+    {
+        return $this->hasMany('App\Models\Category', 'parent_id', 'id');
+    }
+
+    public static function getListByParent()
+    {
+        return Category::with('children')->whereNull('parent_id')->get();
+    }
 }
