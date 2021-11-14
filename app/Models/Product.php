cb6ec0a4c8fc00aac3652a0ebc0e34eec80a52f7
Nhật An <nhatan14042000@gmail.com>
2021-11-14 18:06:45 +0700

    Add method get by slug

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,7 +11,13 @@ class Product extends Model
     protected $table = 'products';
     protected $fillable = ['title', 'description', 'quantity', 'status', 'slug', 'images', 'price', 'sold', 'discount', 'category_id'];
 
-    public function category() {
-        return $this->hasOne('App\Models\Category','id', 'category_id');
+    public function category()
+    {
+        return $this->hasOne('App\Models\Category', 'id', 'category_id');
+    }
+
+    public static function getBySlug($slug)
+    {
+        return Product::where('slug', $slug)->first();
     }
 }
