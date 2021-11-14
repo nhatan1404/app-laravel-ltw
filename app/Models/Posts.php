b5381797496def3cbe637ecd6d9600dd9181aeae
Nhật An <nhatan14042000@gmail.com>
2021-11-14 19:19:57 +0700

    Add get by slug

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -11,15 +11,23 @@ class Posts extends Model
     protected $table = 'posts';
     protected $fillable = ['title', 'description', 'content', 'slug', 'thumbnail', 'status', 'category_id', 'user_id'];
 
-    public function category(){
-        return $this->hasOne('App\Models\PostsCategory','id','category_id');
+    public function category()
+    {
+        return $this->hasOne('App\Models\PostsCategory', 'id', 'category_id');
     }
 
-    public function author(){
-        return $this->hasOne('App\Models\User','id','user_id');
+    public function author()
+    {
+        return $this->hasOne('App\Models\User', 'id', 'user_id');
     }
 
-    public static function getList() {
-        return Posts::with(['category','author'])->orderBy('id','DESC')->paginate(15);
+    public static function getList()
+    {
+        return Posts::with(['category', 'author'])->orderBy('id', 'DESC')->paginate(15);
+    }
+
+    public static function getBySlug($slug)
+    {
+        return Posts::where('slug', $slug)->first();
     }
 }
