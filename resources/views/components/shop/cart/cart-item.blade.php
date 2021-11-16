e61cb811580432aaad7939065ef02cb87cf3128f
Nhật An <nhatan14042000@gmail.com>
2021-11-16 20:49:21 +0700

    Add component cart item

---
+++ resources/views/components/shop/cart/cart-item.blade.php
@@ -0,0 +1,24 @@
+<tr class="text-center ftco-animated" data-row="{{ $item->id }}">
+    <td class="product-remove"><a href="javascript:void(0)" onClick="removeCart({{ $item->id }})"><span
+                class="ion-ios-close"></span></a></td>
+    <td class="image-prod">
+        <div class="img" style="background-image:url('{{ $item->product->images }}');">
+        </div>
+    </td>
+
+    <td class="product-name">
+        <h3>{{ $item->product->title }}</h3>
+    </td>
+
+    <td class="price">{{ $item->product->price }}</td>
+
+    <td class="quantity">
+        <div class="input-group mb-3">
+            <input type="text" name="quantity" data-id="{{ $item->id }}" class="quantity form-control input-number"
+                value="{{ $item->quantity }}" min="1" max="100">
+        </div>
+    </td>
+
+    <td class="total" data-price="{{ $item->id }}">
+        {{ Helpers::formatCurrency($item->product->price * $item->quantity) }} đ</td>
+</tr>
