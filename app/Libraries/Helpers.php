78e310d8bed245bdac5d1f3396560e9e2b09c4b5
Nhật An <nhatan14042000@gmail.com>
2021-11-16 10:42:12 +0700

    Optimize code

--- app/Libraries/Helpers.php
+++ app/Libraries/Helpers.php
@@ -4,41 +4,8 @@ use App\Models\Category;
 
 class Helpers
 {
-
-  public static function getHeaderCategory()
+  public static function getListMenuCategory()
   {
-    $categories = Category::getParentCategories();
-?>
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Danh Mục</a>
-      <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-        <?php
-        foreach ($categories as $parent) {
-          if (count($parent->children) == 0) {
-        ?>
-            <li><a class="dropdown-item" href=""><?php echo $parent->title ?></a></li>
-          <?php
-          } else {
-          ?>
-            <li class="dropdown-submenu">
-              <a class="dropdown-item dropdown-toggle" href="<?php echo route('product-by-category', $parent->slug) ?>"><?php echo $parent->title ?></a>
-              <ul class="dropdown-menu">
-                <?php
-                foreach ($parent->children as $child) {
-                ?>
-                  <li><a class="dropdown-item" href="<?php echo route('product-by-category', $child->slug) ?>"><?php echo $child->title ?></a></li>
-
-                <?php
-                }
-                ?>
-              </ul>
-            </li>
-        <?php
-          }
-        }
-        ?>
-      </ul>
-    </li>
-<?php
+    return Category::getParentCategories();
   }
 }
