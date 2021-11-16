860d9ce3ee4198a0fe3756a4e3c570f1694fee3a
Nhật An <nhatan14042000@gmail.com>
2021-11-16 20:53:32 +0700

    Add format currency

--- app/Libraries/Helpers.php
+++ app/Libraries/Helpers.php
@@ -1,11 +1,24 @@
 <?php
 
+use App\Models\Cart;
 use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
 
 class Helpers
 {
+
+  public static function formatCurrency($currency)
+  {
+    return number_format($currency, 0, ',', '.');
+  }
+
   public static function getListMenuCategory()
   {
     return Category::getParentCategories();
+  }
+
+  public static function getCartCount()
+  {
+    return Cart::where('user_id', Auth::id())->first()->count;
   }
 }
