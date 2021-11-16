9a1b90901b02f49c14dd3d94a898c65aae945c0e
Nhật An <nhatan14042000@gmail.com>
2021-11-16 07:31:10 +0700

    Update using component

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -1,124 +1,56 @@
 @extends('admin.layouts.app')
 @section('title', 'Sửa Tài Khoản')
+
 @section('content')
-
-<div class="row">
-  <div class="col-lg-10 mx-auto">
-    <div class="card">
-      <h5 class="card-header">Sửa Tài Khoản</h5>
-      <div class="card-body">
-        <form method="post" action="{{route('user.update',$user->id)}}">
-          @csrf
-          @method('PATCH')
-          <div class="row">
+    <x-Admin.Form.Edit name="Tài Khoản" route="user.update" :id="$user->id">
+        <div class="row">
             <div class="col">
-              <div class="form-group">
-                <label for="inputLastname" class="col-form-label">Họ: </label>
-                <input id="inputLastname" type="text" name="lastname" placeholder="Nhập họ" value="{{$user->lastname}}" class="form-control">
-                @error('lastname')
-                <span class="text-danger">{{$message}}</span>
-                @enderror
-              </div>
+                <x-Admin.Form.Input name="Họ" property="lastname" placeholder="Nhập họ" value="{{ $user->lastname }}" />
             </div>
             <div class="col">
-              <div class="form-group">
-                <label for="inputFirstname" class="col-form-label">Tên: </label>
-                <input id="inputFirstname" type="text" name="firstname" placeholder="Nhập tên" value="{{$user->firstname}}" class="form-control">
-                @error('firstname')
-                <span class="text-danger">{{$message}}</span>
-                @enderror
-              </div>
+                <x-Admin.Form.Input name="Tên" property="firstname" placeholder="Nhập tên"
+                    value="{{ $user->firstname }}" />
             </div>
-          </div>
+        </div>
 
-          <div class="row">
+        <div class="row">
             <div class="col">
-              <div class="form-group">
-                <label for="inputEmail" class="col-form-label">Email: </label>
-                <input id="inputEmail" type="email" name="email" placeholder="Nhập email" value="{{$user->email}}" class="form-control">
-                @error('email')
-                <span class="text-danger">{{$message}}</span>
-                @enderror
-              </div>
-            </div>
-
-            <div class="col">
-              <div class="form-group">
-                <label for="inputTelephone" class="col-form-label">Điện Thoại:</label>
-                <input id="inputTelephone" type="text" name="telephone" placeholder="Nhập số điện thoại" value="{{$user->telephone}}" class="form-control">
-                @error('telephone')
-                <span class="text-danger">{{$message}}</span>
-                @enderror
-              </div>
-            </div>
-          </div>
-
-          {{-- <div class="form-group">
-            <label for="inputPassword" class="col-form-label">Password</label>
-            <input id="inputPassword" type="password" name="password" placeholder="Enter password" value="{{$user->password}}" class="form-control">
-            @error('password')
-            <span class="text-danger">{{$message}}</span>
-            @enderror
-          </div>--}}
-
-          <div class="form-group">
-            <label for="inputAvatar" class="col-form-label">Ảnh Đại Diện: </label>
-            <div class="input-group">
-              <span class="input-group-btn">
-                <a id="lfm" data-input="inputAvatar" data-preview="holder" class="btn btn-primary">
-                  <i class="fas fa-upload"></i> Chọn
-                </a>
-              </span>
-              <input id="inputAvatar" class="form-control" type="text" name="avatar" value="{{$user->avatar}}">
-            </div>
-            <img id="holder" style="margin-top:15px;max-height:100px;">
-            @error('avatar')
-            <span class="text-danger">{{$message}}</span>
-            @enderror
-          </div>
-         
-          <div class="row">
-            <div class="col">
-              <div class="form-group">
-                <label for="role" class="col-form-label">Chức Vụ:</label>
-                <select name="role" class="form-control">
-                  <option value="">Chọn chức vụ</option>
-                  @foreach($roles as $role)
-                  <option value="{{$role}}" {{(($user->role == $role) ? 'selected' : '')}}>{{ucwords($role)}}</option>
-                  @endforeach
-                </select>
-                @error('role')
-                <span class="text-danger">{{$message}}</span>
-                @enderror
-              </div>
+                <x-Admin.Form.Input name="Email" property="email" type="email" placeholder="Nhập email"
+                    value="{{ $user->email }}" />
             </div>
             <div class="col">
-              <div class="form-group">
-                <label for="status" class="col-form-label">Trạng thái: </label>
-                <select name="status" class="form-control">
-                  <option value="active" {{(($user->status=='active') ? 'selected' : '')}}>Hoạt động</option>
-                  <option value="inactive" {{(($user->status=='inactive') ? 'selected' : '')}}>Không hoạt động</option>
-                </select>
-                @error('status')
-                <span class="text-danger">{{$message}}</span>
-                @enderror
-              </div>
+                <x-Admin.Form.Input name="Điện Thoại" property="telephone" placeholder="Nhập số điện thoại"
+                    value="{{ $user->telephone }}" />
             </div>
-          </div>
-          <div class="form-group mb-3">
-            <button class="btn btn-success" type="submit">Update</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
+        </div>
 
+        <x-Admin.Form.InputImage name="Ảnh đại diện" property="avatar" :value="$user->avatar" />
+
+        <div class="row">
+            <div class="col">
+                <x-Admin.Form.Select name="Chức vụ" property="role">
+                    @foreach ($roles as $role)
+                        <option value="{{ $role }}" {{ $user->role == $role ? 'selected' : '' }}>
+                            {{ ucwords($role) }}</option>
+                    @endforeach
+                </x-Admin.Form.Select>
+            </div>
+            <div class="col">
+                <x-Admin.Form.Select name="Trạng thái" property="status">
+                    @foreach ($roles as $role)
+                        <option value="active" {{ $user->status == 'active' ? 'selected' : '' }}>Hoạt động</option>
+                        <option value="inactive" {{ $user->status == 'inactive' ? 'selected' : '' }}>Không hoạt động
+                        </option>
+                    @endforeach
+                </x-Admin.Form.Select>
+            </div>
+        </div>
+    </x-Admin.Form.Edit>
 @endsection
 
 @push('scripts')
-<script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
-<script>
-  $('#lfm').filemanager();
-</script>
+    <script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
+    <script>
+        $('#lfm').filemanager();
+    </script>
 @endpush
