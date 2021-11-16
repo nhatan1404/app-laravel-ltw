d17861b72a66407be78da26b6098553685441e93
Nhật An <nhatan14042000@gmail.com>
2021-11-16 07:17:44 +0700

    Update method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use App\Models\Product;
 use App\Models\Category;
 use App\Models\Posts;
 use App\Models\PostsCategory;
+use Auth;
 
 class HomeController extends Controller
 {
@@ -25,9 +26,15 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('shop.home');
+        $products = Product::orderBy('created_at', 'DESC')->where('status', 'active')->paginate(8);
+
+        if ($request->ajax()) {
+            $html = view('shop.product.list', compact('products'))->render();
+            return response()->json($html);
+        }
+        return view('shop.home', compact('products'));
     }
 
     public function about()
@@ -42,25 +49,75 @@ class HomeController extends Controller
 
     public function productList()
     {
-        $products = Product::all();
-        return view('shop.product.list', compact('products'));
+        $products = Product::orderBy('created_at', 'DESC')->where('status', 'active')->paginate(8);
+        return view('shop.product.index', compact('products'));
     }
 
     public function productDetail($slug)
     {
-        $product = Product::findorFail($slug);
-        return view('shop.product.detail', compact('product'));
+        $product = Product::getBySlug($slug);
+        $related_products = Category::find($product->category_id)->products->take(4);
+        return view('shop.product.detail', compact('product', 'related_products'));
     }
 
-    public function postSList()
+    public function productByCategory($slug)
     {
-        $posts = Posts::all();
-        return view('shop.posts.list', compact('posts'));
+        $category = Category::getBySlug($slug);
+        $products = $category->products()->paginate(8);
+        if ($category->parent == null) {
+            $children_id = Category::getChildrenIds($category->id);
+            $products = Product::whereIn('category_id', $children_id)->paginate(8);
+        }
+        return view('shop.product.index', compact('products', 'category'));
+    }
+
+    public function postsList()
+    {
+        $posts = Posts::orderBy('created_at', 'DESC')->where('status', 'active')->paginate(5);
+        $recent_posts = Posts::all()->random(3);
+        $categories = PostsCategory::getParentCategories();
+        return view('shop.posts.list', compact('posts', 'recent_posts', 'categories'));
     }
 
     public function postsDetail($slug)
     {
         $post = Posts::getBySlug($slug);
-        return view('shop.posts.detail', compact('post'));
+        $recent_posts = PostsCategory::find($post->category_id)->posts->take(3);
+        $categories =  PostsCategory::getParentCategories();
+        return view('shop.posts.detail', compact('post', 'recent_posts', 'categories'));
+    }
+
+    public function postsByCategory($slug)
+    {
+        $category = PostsCategory::getBySlug($slug);
+        $posts = $category->posts()->paginate(5);
+        $recent_posts = Posts::all()->random(3);
+        $categories = PostsCategory::getParentCategories();
+        return view('shop.posts.list', compact('posts', 'recent_posts', 'categories'));
+    }
+
+    public function login()
+    {
+        return view('shop.user.login');
+    }
+
+    public function register()
+    {
+        return view('shop.user.register');
+    }
+
+    public function profile($id)
+    {
+        $user = Auth::user();
+        return view('shop.user.profile', compact('user'));
+    }
+
+    public function cart()
+    {
+        return view('shop.user.cart');
+    }
+    public function checkout()
+    {
+        return view('shop.user.checkout');
     }
 }
