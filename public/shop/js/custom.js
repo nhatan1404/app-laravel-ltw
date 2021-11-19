0d384c6c3d41c498057a1f0f0d6099b9d0b133b5
Nhật An <nhatan14042000@gmail.com>
2021-11-19 08:50:46 +0700

    Fix bug

--- public/shop/js/custom.js
+++ public/shop/js/custom.js
@@ -14,77 +14,135 @@ const notyf = new Notyf({
 
 function addCart(id) {
     const cart_count = $("#cart_count");
+    const regex = /^\/product\/.*$/g;
+    let quantity = 1;
+    const path = window.location.pathname;
+    if (regex.test(path)) {
+        quantity = $("#quantity").val();
+    }
+    
     $.ajax({
         headers: {
             "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
         },
         type: "get",
         data: {
-            quantity: 1,
+            quantity,
         },
         url: `${window.location.origin}/cart/${id}`,
         success: function (response) {
-            const { total, isUpdate } = response;
+            console.log(response);
+            const { count, isUpdate } = response;
             if (isUpdate) {
                 cart_count.html(
-                    `<span class="icon-shopping_cart"></span>[${total}]`
+                    `<span class="icon-shopping_cart"></span>[${count}]`
                 );
             }
             notyf.open({
                 type: "info",
                 message: "Thêm vào giỏ hàng thành công",
             });
+
+            const path = window.location.pathname.substr(1, 4);
+            if (path === "cart" && count > 0) {
+                setTimeout(() => {
+                    location.reload();
+                }, 1500);
+            }
         },
         error: function (error) {
+            if (error.status === 401) {
+                window.location.href = "/user/login";
+                return;
+            }
             notyf.open({
                 type: "error",
                 message: "Có lỗi xảy ra!",
                 duration: 3000,
             });
-            //console.log(error.responseJSON);
+            console.log(error);
         },
     });
 }
 
-(function updateCart() {
-    const input = $("input.quantity[name='quantity']");
-    input.donetyping(function () {
-        const id = input.data("id");
-        const quantity = parseInt(input.val());
-        if (quantity == 0) return false;
+let timeout = null;
+
+function updateCart(event, id) {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+        const quantity = parseInt(event.target.value);
+        if (quantity == 0) {
+            notyf.open({
+                type: "error",
+                message: "Số lượng phải lớn hơn 0",
+                duration: 3000,
+            });
+            return false;
+        }
         $.ajax({
             headers: {
                 "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
             },
             data: {
-                quantity: quantity,
+                quantity,
             },
             type: "put",
             url: `${window.location.origin}/cart/${id}`,
             success: function (response) {
-                const total = $(`[data-price="${id}"]`);
-                total.text(
-                    response.total.toLocaleString("it-IT", {
+                console.log(response);
+                const { total_item, total } = response;
+                const total_price_item = $(`[data-price="${id}"]`);
+
+                total_price_item.text(
+                    total_item.toLocaleString("it-IT", {
                         style: "currency",
                         currency: "VND",
                     })
                 );
+
+                const subtotal_price = $("#subtotal");
+                subtotal_price.text(
+                    total.toString().toLocaleString("it-IT", {
+                        style: "currency",
+                        currency: "VND",
+                    })
+                );
+
+                const total_price = $("#total-price");
+                total_price.text(
+                    total
+                        ? total.toLocaleString("it-IT", {
+                              style: "currency",
+                              currency: "VND",
+                          })
+                        : "0 VND"
+                );
+
                 notyf.open({
                     type: "info",
                     message: "Cập nhật giỏ hàng thành công!",
                 });
             },
             error: function (error) {
+                if (error.status === 401) {
+                    window.location.href = "/login";
+                    return;
+                }
+                const { type } = error.responseJSON;
+                const message =
+                    type && type == "quantity"
+                        ? "Số lượng lớn hơn số lượng hàng có sẵn"
+                        : "Có lỗi xảy ra";
                 notyf.open({
                     type: "error",
-                    message: "Có lỗi xảy ra!",
+                    message,
                     duration: 3000,
                 });
                 //console.log(error.responseJSON);
             },
         });
-    }, 1000);
-})();
+    }, 2000);
+}
 
 function removeCart(id) {
     const cart_count = $("#cart_count");
@@ -98,21 +156,50 @@ function removeCart(id) {
         type: "delete",
         url: `${window.location.origin}/cart/${id}`,
         success: function (response) {
-            let total = parseInt(cart_count.text().replace(/[^0-9]/g, "_"));
-            total = total > 1 ? total - 1 : 0;
+            const { total } = response;
+            let count = parseInt(cart_count.text().replace(/[^0-9]/g, "_"));
+            count = count > 1 ? count - 1 : 0;
             row.fadeOut(400, function () {
                 row.remove();
             });
             cart_count.html(
-                `<span class="icon-shopping_cart"></span>[${total}]`
+                `<span class="icon-shopping_cart"></span>[${count}]`
             );
+            const subtotal_price = $("#subtotal");
+            subtotal_price.text(
+                total
+                    ? total.toLocaleString("it-IT", {
+                          style: "currency",
+                          currency: "VNĐ",
+                      })
+                    : "0 VND"
+            );
+
+            const total_price = $("#total-price");
+            total_price.text(
+                total
+                    ? total.toLocaleString("it-IT", {
+                          style: "currency",
+                          currency: "VNĐ",
+                      })
+                    : "0 VND"
+            );
+
             notyf.open({
                 type: "info",
                 message: "Cập nhật giỏ hàng thành công!",
             });
-            console.log(response);
+            if (!total) {
+                setTimeout(() => {
+                    location.reload();
+                }, 5500);
+            }
         },
         error: function (error) {
+            if (error.status === 401) {
+                window.location.href = "/login";
+                return;
+            }
             notyf.open({
                 type: "error",
                 message: "Có lỗi xảy ra!",
@@ -122,3 +209,142 @@ function removeCart(id) {
         },
     });
 }
+
+$("#province").change(function () {
+    const id = $(this).find("option:selected").val();
+    $.ajax({
+        headers: {
+            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+        },
+        type: "get",
+        url: `${window.location.origin}/address/districts/${id}`,
+        success: function (data) {
+            const { districts } = data;
+            const districtSelect = $("#district");
+            districtSelect.empty();
+
+            const wardSelect = $("#ward");
+            wardSelect.empty();
+
+            districtSelect.append(
+                '<option value="">Chọn thành phố/quận</option>'
+            );
+
+            wardSelect.append('<option value="">Chọn thành phường/xã</option>');
+
+            districts.forEach((district) => {
+                districtSelect.append(
+                    `<option value="${district.id}">${district.name}</option>`
+                );
+            });
+        },
+        error: function (error) {
+            if (error.status === 401) {
+                window.location.href = "/login";
+                return;
+            }
+            notyf.open({
+                type: "error",
+                message: "Không thể lấy danh sách thành phố/quận!",
+                duration: 3000,
+            });
+        },
+    });
+});
+
+$("#district").change(function () {
+    const id = $(this).find("option:selected").val();
+    $.ajax({
+        headers: {
+            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+        },
+        type: "get",
+        url: `${window.location.origin}/address/wards/${id}`,
+        success: function (data) {
+            const { wards } = data;
+            const wardSelect = $("#ward");
+            wardSelect.empty();
+            wardSelect.append('<option value="">Chọn thành phường/xã</option>');
+
+            wards.forEach((ward) => {
+                wardSelect.append(
+                    `<option value="${ward.id}">${ward.name_with_type}</option>`
+                );
+            });
+        },
+        error: function (error) {
+            if (error.status === 401) {
+                window.location.href = "/login";
+                return;
+            }
+            notyf.open({
+                type: "error",
+                message: "Không thể lấy danh sách thành phường/xã!",
+                duration: 3000,
+            });
+        },
+    });
+});
+
+$("#order").click(function () {
+    const firstname = $('input[name="firstname"]').val().trim();
+    const lastname = $('input[name="lastname"]').val().trim();
+    const province = $("#province").find("option:selected").text().trim();
+    const district = $("#district").find("option:selected").text().trim();
+    const ward = $("#ward").find("option:selected").text().trim();
+    const address = $('input[name="address"]').val().trim();
+    const telephone = $('input[name="telephone"]').val().trim();
+    const email = $('input[name="email"]').val().trim();
+    const note = $('textarea[name="note"]').val().trim();
+    const data = {
+        firstname,
+        lastname,
+        province,
+        district,
+        ward,
+        address,
+        telephone,
+        email,
+        note,
+    };
+    $.ajax({
+        headers: {
+            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+        },
+        type: "post",
+        data,
+        url: `${window.location.origin}/order`,
+        success: function (data) {
+            notyf.open({
+                type: "info",
+                message: "Đặt hàng thành công",
+                duration: 3000,
+            });
+
+            setTimeout(function () {
+                window.location.href = `${window.location.origin}/cart`;
+            }, 4000);
+        },
+        error: function (error) {
+            if (error.status === 401) {
+                window.location.href = "/login";
+                return;
+            }
+            if (error.message == "cart-empty") {
+                notyf.open({
+                    type: "error",
+                    message: "Giỏ hàng trống",
+                    duration: 3000,
+                });
+                setTimeout(function () {
+                    location.reload();
+                }, 3500);
+            }
+            notyf.open({
+                type: "error",
+                message: "Đặt hàng không thành công.",
+                duration: 3000,
+            });
+        },
+    });
+});
