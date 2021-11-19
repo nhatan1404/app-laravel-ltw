751e5c9b4b50dfbf3cec0a8392f61fad9f2f2449
Nhật An <nhatan14042000@gmail.com>
2021-11-19 09:29:06 +0700

    Update table order

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -5,23 +5,45 @@
     @php
     $columns = [
         'id' => 'ID',
-        'title' => 'Tiêu Đề',
-        'description' => 'Mô Tả',
-        'slug' => 'Đường Dẫn',
+        'order_number' => 'Mã Đơn',
+        'fullname' => 'Họ Tên',
+        'address' => 'Địa Chỉ',
+        'email' => 'Email',
+        'telephone' => 'Số Điện Thoại',
+        'status' => 'Trạng Thái',
+        'total' => 'Tổng',
+        'note' => 'Ghi chú',
         '' => '',
     ];
     @endphp
 
-    <x-Admin.Table name="danh mục sản phẩm" :columns="$columns" create="category.create" :value="$categories">
-        @foreach ($categories as $category)
+    <x-Admin.Table name="đơn đặt hàng" :columns="$columns" create="order.create" :value="$orders">
+        @foreach ($orders as $order)
             <tr>
-                <td>{{ $category->id }}</td>
-                <td>{{ $category->title }}</td>
-                <td>{{ $category->description }}</td>
-                <td>{{ $category->slug }}</td>
+                <td>{{ $order->id }}</td>
+                <td>{{ $order->order_number }}</td>
+                <td>{{ $order->fullname }}</td>
+                <td>{{ $order->address }}</td>
+                <td>{{ $order->email }}</td>
+                <td>{{ $order->telephone }}</td>
+                <td class="text-center">
+                    @if ($order->status == 'new')
+                        <span class="badge badge-primary">{{ $order->status }}</span>
+                    @elseif($order->status=='accepted')
+                        <span class="badge badge-warning">{{ $order->status }}</span>
+                    @elseif($order->status=='cancel')
+                        <span class="badge badge-danger">{{ $order->status }}</span>
+                    @elseif($order->status=='done')
+                        <span class="badge badge-success">{{ $order->status }}</span>
+                    @else
+                        <span class="badge badge-info">{{ $order->status }}</span>
+                    @endif
+                </td>
+                <td>{{ Helpers::formatCurrency($order->total) }} VNĐ </td>
+                <td>{{ $order->note != null ? $order->note : 'Trống...' }}</td>
                 </td>
                 <td>
-                    <x-Admin.ButtonAction :id="$category->id" edit="category.edit" delete="category.destroy" />
+                    <x-Admin.ButtonAction :id="$order->id" edit="order.edit" delete="order.destroy" />
                 </td>
             </tr>
         @endforeach
