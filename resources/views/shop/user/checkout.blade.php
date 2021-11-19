150e842f27db78320490dae11be5860d6b7728cf
Nhật An <nhatan14042000@gmail.com>
2021-11-19 09:09:37 +0700

    Update view checkout

--- resources/views/shop/user/checkout.blade.php
+++ resources/views/shop/user/checkout.blade.php
@@ -2,14 +2,15 @@
 @section('title', 'Liên hệ')
 
 @section('content')
-    <div class="hero-wrap hero-bread" style="background-image: url('images/bg_1.jpg');">
+    <div class="hero-wrap hero-bread" style="background-image: url('{{ asset('shop/images/bg_1.jpg') }}');">
         <div class="container">
             <div class="row no-gutters slider-text align-items-center justify-content-center">
                 <div class="col-md-9 ftco-animate text-center">
-                    <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span>
-                        <span>Checkout</span>
+                    <p class="breadcrumbs"><span class="mr-2"><a href="{{ route('home') }}">Trang
+                                Chủ</a></span>
+                        <span>Thanh Toán</span>
                     </p>
-                    <h1 class="mb-0 bread">Checkout</h1>
+                    <h1 class="mb-0 bread">Thanh Toán</h1>
                 </div>
             </div>
         </div>
@@ -20,85 +21,102 @@
             <div class="row justify-content-center">
                 <div class="col-xl-7 ftco-animate">
                     <form action="#" class="billing-form">
-                        <h3 class="mb-4 billing-heading">Billing Details</h3>
+                        <h3 class="mb-4 billing-heading">Chi tiết thanh toán</h3>
                         <div class="row align-items-end">
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="firstname">Firt Name</label>
-                                    <input type="text" class="form-control" placeholder="">
+                                    <label for="lastname">Họ:</label>
+                                    <input type="text" name="lastname" class="form-control" placeholder=""
+                                        value="{{ $user->lastname }}">
                                 </div>
                             </div>
+
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="lastname">Last Name</label>
-                                    <input type="text" class="form-control" placeholder="">
+                                    <label for="firstname">Tên:</label>
+                                    <input type="text" name="firstname" class="form-control" placeholder=""
+                                        value="{{ $user->firstname }}">
                                 </div>
                             </div>
+
                             <div class="w-100"></div>
-                            <div class="col-md-12">
+                            <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="country">State / Country</label>
+                                    <label for="country">Tỉnh: </label>
                                     <div class="select-wrap">
                                         <div class="icon"><span class="ion-ios-arrow-down"></span></div>
-                                        <select name="" id="" class="form-control">
-                                            <option value="">France</option>
-                                            <option value="">Italy</option>
-                                            <option value="">Philippines</option>
-                                            <option value="">South Korea</option>
-                                            <option value="">Hongkong</option>
-                                            <option value="">Japan</option>
+                                        <select name="province" id="province" class="form-control">
+                                            <option value="">Chọn tỉnh</option>
+                                            @foreach ($provinces as $province)
+                                                <option value="{{ $province->id }}"
+                                                    {{ $province->id == $user->address->province->id ? ' selected' : '' }}>
+                                                    {{ $province->name_with_type }}</option>
+                                            @endforeach
                                         </select>
                                     </div>
                                 </div>
                             </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="country">Thành phố/Quận: </label>
+                                    <div class="select-wrap">
+                                        <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                                        <select name="district" id="district" class="form-control">
+                                            @foreach (Helpers::getDistricts($user->address->province->id) as $district)
+                                                <option value="{{ $district->id }}"
+                                                    {{ $district->id == $user->address->district->id ? ' selected="selected"' : '' }}>
+                                                    {{ $district->name_with_type }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+
                             <div class="w-100"></div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="streetaddress">Street Address</label>
-                                    <input type="text" class="form-control" placeholder="House number and street name">
+                                    <label for="country">Phường/Xã: </label>
+                                    <div class="select-wrap">
+                                        <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                                        <select name="ward" id="ward" class="form-control">
+                                            @foreach (Helpers::getWards($user->address->district->id) as $ward)
+                                                <option value="{{ $ward->id }}"
+                                                    {{ $ward->id == $user->address->ward->id ? ' selected' : '' }}>
+                                                    {{ $ward->name_with_type }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <input type="text" class="form-control"
-                                        placeholder="Appartment, suite, unit etc: (optional)">
+                                    <label for="streetaddress">Địa chỉ: </label>
+                                    <input type="text" name="address" class="form-control"
+                                        placeholder="Nhập số nhà và tên đường" value="{{ $user->address->address }}">
                                 </div>
                             </div>
+
                             <div class="w-100"></div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="towncity">Town / City</label>
-                                    <input type="text" class="form-control" placeholder="">
+                                    <label for="phone">Số điện thoại:</label>
+                                    <input type="text" name="telephone" class="form-control" placeholder=""
+                                        value="{{ $user->telephone }}">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="postcodezip">Postcode / ZIP *</label>
-                                    <input type="text" class="form-control" placeholder="">
+                                    <label for="emailaddress">Email:</label>
+                                    <input type="text" name="email" class="form-control" placeholder=""
+                                        value="{{ $user->email }}">
                                 </div>
                             </div>
-                            <div class="w-100"></div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="phone">Phone</label>
-                                    <input type="text" class="form-control" placeholder="">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="emailaddress">Email Address</label>
-                                    <input type="text" class="form-control" placeholder="">
-                                </div>
-                            </div>
+
                             <div class="w-100"></div>
                             <div class="col-md-12">
-                                <div class="form-group mt-4">
-                                    <div class="radio">
-                                        <label class="mr-3"><input type="radio" name="optradio"> Create an
-                                            Account?
-                                        </label>
-                                        <label><input type="radio" name="optradio"> Ship to different address</label>
-                                    </div>
+                                <div class="form-group">
+                                    <label for="emailaddress">Ghi chú:</label>
+                                    <textarea name="note" class="form-control"></textarea>
                                 </div>
                             </div>
                         </div>
@@ -108,63 +126,47 @@
                     <div class="row mt-5 pt-3">
                         <div class="col-md-12 d-flex mb-5">
                             <div class="cart-detail cart-total p-3 p-md-4">
-                                <h3 class="billing-heading mb-4">Cart Total</h3>
-                                <p class="d-flex">
-                                    <span>Subtotal</span>
-                                    <span>$20.60</span>
-                                </p>
-                                <p class="d-flex">
-                                    <span>Delivery</span>
-                                    <span>$0.00</span>
-                                </p>
-                                <p class="d-flex">
-                                    <span>Discount</span>
-                                    <span>$3.00</span>
-                                </p>
-                                <hr>
-                                <p class="d-flex total-price">
-                                    <span>Total</span>
-                                    <span>$17.60</span>
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                            <div class="cart-detail p-3 p-md-4">
-                                <h3 class="billing-heading mb-4">Payment Method</h3>
-                                <div class="form-group">
-                                    <div class="col-md-12">
-                                        <div class="radio">
-                                            <label><input type="radio" name="optradio" class="mr-2"> Direct Bank
-                                                Tranfer</label>
-                                        </div>
-                                    </div>
+                                <h3 class="d-flex justify-content-between align-items-center mb-3">
+                                    <span class="text-muted">Giỏ hàng</span>
+                                    <span class="badge badge-primary badge-pill mr-3">{{ $carts->count }}</span>
+                                </h3>
+                                <ul class="list-group mb-3">
+                                    @foreach ($carts->items as $item)
+                                        <li class="list-group-item d-flex justify-content-between lh-condensed">
+                                            <div>
+                                                <h6 class="my-0">{{ $item->product->title }}</h6>
+                                                <small class="text-muted">Số lượng: {{ $item->quantity }}</small>
+                                            </div>
+                                            <span
+                                                class="text-muted">{{ Helpers::formatCurrency($item->quantity * $item->product->price) }}
+                                                VNĐ</span>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                                <h3 class="billing-heading ml-3 mt-3 mb-3">Tổng giỏ hàng</h3>
+                                <div class="ml-2">
+                                    <p class="d-flex">
+                                        <span>Tổng tiền hàng</span>
+                                        <span id="subtotal">{{ Helpers::formatCurrency($carts->total) }} VNĐ</span>
+                                    </p>
+                                    <p class="d-flex">
+                                        <span>Phí vận chuyển</span>
+                                        <span>0 VNĐ</span>
+                                    </p>
+                                    <p class="d-flex">
+                                        <span>Mã giảm giá</span>
+                                        <span>0 VNĐ</span>
+                                    </p>
+                                    <hr>
+                                    <p class="d-flex total-price">
+                                        <span>Tổng số tiền</span>
+                                        <span>{{ Helpers::formatCurrency($carts->total) }} VNĐ</span>
+                                    </p>
+                                    <p>
+                                        <a href="javascript:void(0)" id="order" class="btn btn-primary py-3 px-4">Thanh
+                                            Toán</a>
+                                    </p>
                                 </div>
-                                <div class="form-group">
-                                    <div class="col-md-12">
-                                        <div class="radio">
-                                            <label><input type="radio" name="optradio" class="mr-2"> Check
-                                                Payment</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-md-12">
-                                        <div class="radio">
-                                            <label><input type="radio" name="optradio" class="mr-2">
-                                                Paypal</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-md-12">
-                                        <div class="checkbox">
-                                            <label><input type="checkbox" value="" class="mr-2"> I have read and
-                                                accept
-                                                the terms and conditions</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <p><a href="#" class="btn btn-primary py-3 px-4">Place an order</a></p>
                             </div>
                         </div>
                     </div>
