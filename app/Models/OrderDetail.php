83f28a06cbd49fade2209cf37563a084c8860c09
Nhật An <nhatan14042000@gmail.com>
2021-11-19 09:26:41 +0700

    Add relation

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -8,6 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class OrderDetail extends Model
 {
     use HasFactory;
-    protected $table = 'order_detail';
+    protected $table = 'order_details';
     protected $fillable = ['order_id', 'product_id', 'quantity', 'price'];
+ 
+    public function order()
+    {
+        return $this->hasOne('App\Models\Order', 'id', 'order_id');
+    }
+
+    public function product()
+    {
+        return $this->hasOne('App\Models\Product', 'id', 'product_id');
+    }
 }
