b30048b0b4b2779d407ca4160d8e51594571e29c
Nhật An <nhatan14042000@gmail.com>
2022-01-10 16:38:30 +0700

    Change class action to circle

--- resources/views/components/admin/button-action.blade.php
+++ resources/views/components/admin/button-action.blade.php
@@ -1,10 +1,10 @@
-<a href="{{ route($show, $id) }}" class="btn btn-primary btn-sm float-left mr-1 btn-action" data-toggle="tooltip"
-    title="Sửa" data-placement="bottom"><i class="fas fa-info-circle"></i></a>
-<a href="{{ route($edit, $id) }}" class="btn btn-warning btn-sm float-left mr-1 btn-action" data-toggle="tooltip"
+<a href="{{ route($show, $id) }}" class="btn btn-primary btn-sm float-left mr-1 btn-circle" data-toggle="tooltip"
+    title="Xem" data-placement="bottom"><i class="fas fa-info-circle"></i></a>
+<a href="{{ route($edit, $id) }}" class="btn btn-warning btn-sm float-left mr-1 btn-circle" data-toggle="tooltip"
     title="Sửa" data-placement="bottom"><i class="fas fa-edit"></i></a>
 <form method="POST" action="{{ route($delete, [$id]) }}">
     @csrf
     @method('delete')
-    <button class="btn btn-danger btn-sm btn-action btnDelete" data-id="{{ $id }}" data-toggle="tooltip"
+    <button class="btn btn-danger btn-sm btn-circle btnDelete" data-id="{{ $id }}" data-toggle="tooltip"
         data-placement="bottom" title="Xoá"><i class="fas fa-trash-alt"></i></button>
 </form>
