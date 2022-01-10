80bc164a7cf16e472491ea1d436e911bc6ad06e7
Nhật An <nhatan14042000@gmail.com>
2022-01-10 17:24:47 +0700

    Add summernote editor

--- resources/views/components/admin/form/textarea.blade.php
+++ resources/views/components/admin/form/textarea.blade.php
@@ -16,3 +16,61 @@
         <span class="text-danger">{{ $message }}</span>
     @enderror
 </div>
+
+@push('scripts')
+    <script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+
+            // Define function to open filemanager window
+            var lfm = function(options, cb) {
+                var route_prefix = (options && options.prefix) ? options.prefix : '/laravel-filemanager';
+                window.open(route_prefix + '?type=' + options.type || 'file', 'FileManager',
+                    'width=900,height=600');
+                window.SetUrl = cb;
+            };
+
+            // Define LFM summernote button
+            var LFMButton = function(context) {
+                var ui = $.summernote.ui;
+                var button = ui.button({
+                    contents: '<i class="note-icon-picture"></i> ',
+                    tooltip: 'Insert image with filemanager',
+                    click: function() {
+
+                        lfm({
+                            type: 'image',
+                            prefix: '/laravel-filemanager'
+                        }, function(lfmItems, path) {
+                            lfmItems.forEach(function(lfmItem) {
+                                context.invoke('insertImage', lfmItem.url);
+                            });
+                        });
+
+                    }
+                });
+                return button.render();
+            };
+
+            // Initialize summernote with LFM button in the popover button group
+            // Please note that you can add this button to any other button group you'd like
+            $('#input{{ ucfirst($property) }}').summernote({
+                placeholder: '{{ $placeholder }}',
+                toolbar: [
+                    ['style', ['style']],
+                    ['font', ['bold', 'underline', 'clear']],
+                    ['color', ['color']],
+                    ['para', ['ul', 'ol', 'paragraph']],
+                    ['table', ['table']],
+                    ['popovers', ['lfm']],
+                    ['view', ['fullscreen', 'codeview', 'help']]
+                ],
+                buttons: {
+                    lfm: LFMButton
+                }
+            })
+        });
+    </script>
+@endpush
