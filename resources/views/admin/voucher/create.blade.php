33dbad7dc167e2b5f950c1c98f99deb4724653c7
Nhật An <nhatan14042000@gmail.com>
2022-01-10 17:38:17 +0700

    Add selected old value

--- resources/views/admin/voucher/create.blade.php
+++ resources/views/admin/voucher/create.blade.php
@@ -6,19 +6,19 @@
         <x-Admin.Form.Input name="Mã" property="code" placeholder="Nhập mã" value="{{ old('code') }}" />
 
         <x-Admin.Form.Select name="Loại" property="type">
-            <option value="fixed">Giá tiền</option>
-            <option value="percent">Phần trăm</option>
+            <option value="fixed" {{ old('type') == 'fixed' ? 'selected' : '' }}>Giá tiền</option>
+            <option value="percent" {{ old('type') == 'percent' ? 'selected' : '' }}>Phần trăm</option>
         </x-Admin.Form.Select>
 
         <x-Admin.Form.Input name="Giá trị" property="value" type="number" placeholder="Nhập giá trị"
             value="{{ old('value') }}" />
-            
+
         <x-Admin.Form.Input name="Số lượt sử dụng" property="time" type="number" placeholder="Nhập số lượt"
             value="{{ old('time') }}" />
 
         <x-Admin.Form.Select name="Trạng thái" property="status">
-            <option value="active">Còn hiệu lực</option>
-            <option value="inactive">Hết hạn</option>
+            <option value="active" {{ old('status') == 'active' ? 'selected' : '' }}>Còn hiệu lực</option>
+            <option value="inactive" {{ old('status') == 'inactive' ? 'selected' : '' }}>Hết hạn</option>
         </x-Admin.Form.Select>
     </x-Admin.Form.Create>
 @endsection
