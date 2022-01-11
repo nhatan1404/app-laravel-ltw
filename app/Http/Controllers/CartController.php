842950e7adc1f645940119d04d41abb2122a1cb7
Nhật An <nhatan14042000@gmail.com>
2022-01-11 07:55:09 +0700

    Update cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,16 +6,26 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
 use App\Models\CartItem;
+use App\Models\Voucher;
+use Helpers;
 use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
-
     public function getListCart()
     {
         $carts = Cart::getCart();
         $user = Auth::user();
-        return view('shop.user.cart', compact('carts', 'user'));
+
+        if (session('coupon')) {
+            $coupon = Voucher::find(session('coupon')['id']);
+            if ($coupon == null || ($coupon && $coupon->time == 0)) {
+                session()->forget('coupon');
+            }
+        }
+
+        $discount_money = Helpers::getDiscountMoney($carts->total ?? 0);
+        return view('shop.user.cart', compact('carts', 'user', 'discount_money'));
     }
 
     public function addToCart(Request $request, $id)
@@ -24,6 +34,10 @@ class CartController extends Controller
 
             $carts = Cart::getCart();
             $product = Product::findOrFail($id);
+
+            if ($product == null) {
+                return response()->json(['message' => 'Không tìm thấy sản phẩm', 'type' => 'product-not-found'], 400);
+            }
 
             if ($request->quantity > $product->quantity) {
                 return response()->json(['message' => 'Số lượng đặt hàng không được lớn hơn số lượng trong kho', 'type' => 'quantity'], 400);
@@ -57,8 +71,7 @@ class CartController extends Controller
                 $cart_item->save();
                 return response()->json(['message' => 'Thêm vào giỏ hàng thành công', 'count' => $cart->count, 'isUpdate' => true], 200);
             }
-        }
-        return response()->json(['error' => 'Error'], 400);
+        } else return response()->json(['message' => 'Có lỗi xảy ra', 'type' => 'request'], 400);
     }
 
     public function updateCart(Request $request, $id)
@@ -80,24 +93,38 @@ class CartController extends Controller
             if ($status) {
                 $carts = Cart::getCart();
                 $total_item = $item->quantity * $item->product->price;
-                return response()->json(['message' => 'Cập nhập giỏ hàng thành công', 'count' => $carts->count, 'total_item' => $total_item, 'total' => $carts->total], 200);
+                $discount_money = Helpers::getDiscountMoney($carts->total);
+                return response()->json(['message' => 'Cập nhập giỏ hàng thành công', 'count' => $carts->count, 'total_item' => $total_item, 'discount_money' => $discount_money, 'total' => $carts->total], 200);
             } else {
                 return response()->json(['message' => 'Không tìm thấy sản phẩm'], 404);
             }
-        }
+        } else return response()->json(['message' => 'Có lỗi xảy ra', 'type' => 'request'], 400);
     }
 
     public function removeCart(Request $request, $id)
     {
         if ($request->ajax()) {
-            $item = CartItem::findOrFail($id);
+            $item = CartItem::find($id);
+
+            if ($item == null) {
+                return response()->json(['message' => 'Không tìm thấy sản phẩm', 'type' => 'item-not-found'], 400);
+            }
+
             $status = $item->delete();
+
             if ($status) {
                 $carts = Cart::getCart();
-                return response()->json(['message' => 'Xoá sản phẩm khỏi giỏ hàng thành công', 'total' => $carts->total], 200);
+
+                if ($carts->total == 0) {
+                    $discount_money = 0;
+                } else {
+                    $discount_money = Helpers::getDiscountMoney($carts->total);
+                }
+
+                return response()->json(['message' => 'Xoá sản phẩm khỏi giỏ hàng thành công', 'discount_money' => $discount_money, 'total' => $carts->total], 200);
             } else {
                 return response()->json(['message' => 'Không tìm thấy sản phẩm'], 404);
             }
-        }
+        } else return response()->json(['message' => 'Có lỗi xảy ra', 'type' => 'request'], 400);
     }
 }
