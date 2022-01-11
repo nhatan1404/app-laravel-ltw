944ca670fd4a36a95c23827616c512157f38576a
Nhật An <nhatan14042000@gmail.com>
2022-01-11 19:56:23 +0700

    Fix link breadcrumb

--- resources/views/shop/user/cart.blade.php
+++ resources/views/shop/user/cart.blade.php
@@ -6,10 +6,10 @@
         <div class="container">
             <div class="row no-gutters slider-text align-items-center justify-content-center">
                 <div class="col-md-9 ftco-animate text-center">
-                    <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span>
-                        <span>Cart</span>
+                    <p class="breadcrumbs"><span class="mr-2"><a href="{{ route('home') }}">Trang Chủ</a></span>
+                        <span>Giỏ Hàng</span>
                     </p>
-                    <h1 class="mb-0 bread">My Cart</h1>
+                    <h1 class="mb-0 bread">Giỏ Hàng</h1>
                 </div>
             </div>
         </div>
