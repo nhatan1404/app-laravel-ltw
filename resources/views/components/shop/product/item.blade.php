10bf139b5a059a7bb5aecb49e5a2c5beab8b3c79
Nhật An <nhatan14042000@gmail.com>
2022-01-11 07:54:40 +0700

    Add slug and remove unused tag

--- resources/views/components/shop/product/item.blade.php
+++ resources/views/components/shop/product/item.blade.php
@@ -6,26 +6,19 @@
             <div class="overlay"></div>
         </a>
         <div class="text py-3 pb-4 px-3 text-center">
-            <h3><a href="#">{{ $product->title }}</a></h3>
+            <h3><a href="{{ route('product-detail', $product->slug) }}">{{ $product->title }}</a></h3>
             <div class="d-flex">
                 <div class="pricing">
-                    <p class="price"><span class="mr-2 price-dc">{{ $product->origin_price }}đ</span><span
-                            class="price-sale">{{ $product->price_after_discount }}</span>đ</span>
+                    <p class="price"><span class="mr-2 price-dc">{{ $product->origin_price }}</span><span
+                            class="price-sale">{{ $product->price_after_discount }}</span></span>
                     </p>
                 </div>
             </div>
             <div class="bottom-area d-flex px-3">
                 <div class="m-auto d-flex">
-                    <a href="javascript:void(0)"
-                        class="add-to-cart d-flex justify-content-center align-items-center text-center">
-                        <span><i class="ion-ios-menu"></i></span>
-                    </a>
                     <a href="javascript:void(0)" onclick="addCart({{ $product->id }})"
                         class="buy-now d-flex justify-content-center align-items-center mx-1">
                         <span><i class="ion-ios-cart"></i></span>
-                    </a>
-                    <a href="javascript:void(0)" class=" heart d-flex justify-content-center align-items-center ">
-                        <span><i class="ion-ios-heart"></i></span>
                     </a>
                 </div>
             </div>
