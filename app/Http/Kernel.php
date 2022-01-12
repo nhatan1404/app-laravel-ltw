25c771405d081c0195b4987e6c7583461b8a7808
Nhật An <nhatan14042000@gmail.com>
2022-01-12 19:40:30 +0700

    Add middlware dashboard access

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,8 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admin' => \App\Http\Middleware\Admin::class,
-        'employee' => \App\Http\Middleware\Employee::class,
+        'dashboard.access' => \App\Http\Middleware\DashboardAccess::class,
         'user' => \App\Http\Middleware\User::class
     ];
 }
